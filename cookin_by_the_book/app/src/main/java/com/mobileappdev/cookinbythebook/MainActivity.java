e8a897f0444895b0b93ac6c9a43b5971788299ce
msicili1 <69369394+matthew-siciliano@users.noreply.github.com>
2021-04-12 19:22:35 -0400

    Added search. The code to disable it is currently commented out because I'm unable to make the search button only appear on a single page--it's currently all or nothing.

--- cookin_by_the_book/app/src/main/java/com/mobileappdev/cookinbythebook/MainActivity.java
+++ cookin_by_the_book/app/src/main/java/com/mobileappdev/cookinbythebook/MainActivity.java
@@ -1,8 +1,13 @@
 package com.mobileappdev.cookinbythebook;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -47,5 +52,21 @@ public class MainActivity extends AppCompatActivity {
             mParent.addView(child, 0);
         }
     }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
 
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+*/
 }
--- cookin_by_the_book/app/src/main/java/com/mobileappdev/cookinbythebook/ui/dashboard/DashboardFragment.java
+++ cookin_by_the_book/app/src/main/java/com/mobileappdev/cookinbythebook/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,14 @@
 package com.mobileappdev.cookinbythebook.ui.dashboard;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -30,6 +35,39 @@ public class DashboardFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        setHasOptionsMenu(true);
         return root;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    /*
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return true;
+    }*/
 }
