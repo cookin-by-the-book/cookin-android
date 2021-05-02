95bdcb7b8fbb87178210030ed4b317db10a544d0
itspaulyg <pgierl@nd.edu>
2021-05-02 17:59:03 -0400

    account info displayed

--- cookin_by_the_book/app/src/main/java/com/mobileappdev/cookinbythebook/navigation_account.java
+++ cookin_by_the_book/app/src/main/java/com/mobileappdev/cookinbythebook/navigation_account.java
@@ -21,14 +21,20 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 
 public class navigation_account extends Fragment {
 
     private Button signOutButton, changePasswordButton;
-    private TextView greeting;
+    private TextView name, emailText, numRecipesText;
     private EditText oldPassword, newPassword, confirmPassword;
     private FirebaseAuth mAuth;
 
@@ -42,15 +48,44 @@ public class navigation_account extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mAuth = FirebaseAuth.getInstance();
-        View root =  inflater.inflate(R.layout.fragment_navigation_account, container, false);
-        greeting = (TextView) root.findViewById(R.id.greeting);
-        FirebaseUser user = mAuth.getCurrentUser();
         final String TAG = "Account";
 
-        if(user != null) {
-            greeting.setText(user.getEmail());
-        }
+        mAuth = FirebaseAuth.getInstance();
+
+        View root =  inflater.inflate(R.layout.fragment_navigation_account, container, false);
+        name = (TextView) root.findViewById(R.id.name);
+        emailText = (TextView) root.findViewById(R.id.email);
+        numRecipesText = (TextView) root.findViewById(R.id.numRecipes);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        String authEmail = user.getEmail();
+
+        Databaser db = new Databaser();
+        db.init();
+        CollectionReference userStore = db.getStore("users");
+
+        userStore.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task0) {
+                if (task0.isSuccessful()) {
+                    for (QueryDocumentSnapshot document0 : task0.getResult()) {
+                        Map<String, Object> dater0 = document0.getData();
+                        String email = (String) dater0.get("email");
+                        if (email != null && email.equals(authEmail)) {
+                            String firstName = (String) dater0.get("firstName");
+                            String lastName = (String) dater0.get("lastName");
+                            ArrayList recipes = (ArrayList) dater0.get("recipes");
+                            String numRecipes = String.valueOf(recipes.size());
+                            name.setText(String.format("%s %s", firstName, lastName));
+                            emailText.setText(email);
+                            numRecipesText.setText(numRecipes);
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "Error getting documents.", task0.getException());
+                }
+            }
+        });
 
         changePasswordButton = (Button) root.findViewById(R.id.changePassword);
         oldPassword = (EditText) root.findViewById(R.id.oldPassword);
