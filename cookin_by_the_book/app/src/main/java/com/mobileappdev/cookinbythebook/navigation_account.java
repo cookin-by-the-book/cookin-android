0a8981d4d71b0db22d8a1149271811afb59dcf66
itspaulyg <pgierl@nd.edu>
2021-05-01 23:58:47 -0400

    change password works

--- cookin_by_the_book/app/src/main/java/com/mobileappdev/cookinbythebook/navigation_account.java
+++ cookin_by_the_book/app/src/main/java/com/mobileappdev/cookinbythebook/navigation_account.java
@@ -3,6 +3,7 @@ package com.mobileappdev.cookinbythebook;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -10,8 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,8 +27,9 @@ import org.w3c.dom.Text;
 
 public class navigation_account extends Fragment {
 
-    private Button signOutButton;
+    private Button signOutButton, changePasswordButton;
     private TextView greeting;
+    private EditText oldPassword, newPassword, confirmPassword;
     private FirebaseAuth mAuth;
 
     @Override
@@ -38,13 +46,57 @@ public class navigation_account extends Fragment {
         View root =  inflater.inflate(R.layout.fragment_navigation_account, container, false);
         greeting = (TextView) root.findViewById(R.id.greeting);
         FirebaseUser user = mAuth.getCurrentUser();
+        final String TAG = "Account";
 
         if(user != null) {
             greeting.setText(user.getEmail());
         }
 
-        signOutButton = (Button) root.findViewById(R.id.signOut);
+        changePasswordButton = (Button) root.findViewById(R.id.changePassword);
+        oldPassword = (EditText) root.findViewById(R.id.oldPassword);
+        newPassword = (EditText) root.findViewById(R.id.newPassword);
+        confirmPassword = (EditText) root.findViewById(R.id.confirmPassword);
 
+        changePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), oldPassword.getText().toString());
+
+                // Prompt the user to re-provide their sign-in credentials
+                user.reauthenticate(credential)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, newPassword.getText().toString());
+                                    Log.d(TAG, confirmPassword.getText().toString());
+                                    if(newPassword.getText().toString().equals(confirmPassword.getText().toString())) {
+                                        user.updatePassword(newPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Log.d(TAG, "Password updated");
+                                                    Toast.makeText(getActivity(), "Password changed successfully", Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    Log.d(TAG, "Error password not updated");
+                                                    Toast.makeText(getActivity(), "Password change failed", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    } else {
+                                        Toast.makeText(getActivity(), "Passwords must match", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Log.d(TAG, "Error auth failed");
+                                }
+                            }
+                        });
+            }
+        });
+
+
+
+        signOutButton = (Button) root.findViewById(R.id.signOut);
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
